2625e1e4365c9e3f3148b3ced3ed8fdc7581788f
Jesse Cui <jessecui@seas.upenn.edu>
2019-12-18 17:54:16 -0500

    Fix typo

--- crime_final_processor.py
+++ crime_final_processor.py
@@ -52,26 +52,26 @@ vacant_crime_df = pd.concat((crime_df_1, crime_df_2, crime_df_3), axis=0)
 
 # Count the number of violent and non-violent crimes before and after
 print("STEP 2a: Count crimes 100 meter radius")
-vacant_crime_df['violent_100_before'] = greened_with_crime_df.apply(lambda row: sum(i <= 400 for i in literal_eval(row['100_before'])), axis = 1)
-vacant_crime_df['violent_100_after'] = greened_with_crime_df.apply(lambda row: sum(i <= 400 for i in literal_eval(row['100_after'])), axis = 1)
-vacant_crime_df['nonviolent_100_before'] = greened_with_crime_df.apply(lambda row: sum(i > 400 for i in literal_eval(row['100_before'])), axis = 1)
-vacant_crime_df['nonviolent_100_after'] = greened_with_crime_df.apply(lambda row: sum(i > 400 for i in literal_eval(row['100_after'])), axis = 1) 
+vacant_crime_df['violent_100_before'] = vacant_crime_df.apply(lambda row: sum(i <= 400 for i in literal_eval(row['100_before'])), axis = 1)
+vacant_crime_df['violent_100_after'] = vacant_crime_df.apply(lambda row: sum(i <= 400 for i in literal_eval(row['100_after'])), axis = 1)
+vacant_crime_df['nonviolent_100_before'] = vacant_crime_df.apply(lambda row: sum(i > 400 for i in literal_eval(row['100_before'])), axis = 1)
+vacant_crime_df['nonviolent_100_after'] = vacant_crime_df.apply(lambda row: sum(i > 400 for i in literal_eval(row['100_after'])), axis = 1) 
 
 print("STEP 2b: Count crimes 200 meter radius")
-vacant_crime_df['violent_200_before'] = greened_with_crime_df.apply(lambda row: sum(i <= 400 for i in literal_eval(row['200_before'])), axis = 1)
-vacant_crime_df['violent_200_after'] = greened_with_crime_df.apply(lambda row: sum(i <= 400 for i in literal_eval(row['200_after'])), axis = 1)
-vacant_crime_df['nonviolent_200_before'] = greened_with_crime_df.apply(lambda row: sum(i > 400 for i in literal_eval(row['200_before'])), axis = 1)
-vacant_crime_df['nonviolent_200_after'] = greened_with_crime_df.apply(lambda row: sum(i > 400 for i in literal_eval(row['200_after'])), axis = 1) 
+vacant_crime_df['violent_200_before'] = vacant_crime_df.apply(lambda row: sum(i <= 400 for i in literal_eval(row['200_before'])), axis = 1)
+vacant_crime_df['violent_200_after'] = vacant_crime_df.apply(lambda row: sum(i <= 400 for i in literal_eval(row['200_after'])), axis = 1)
+vacant_crime_df['nonviolent_200_before'] = vacant_crime_df.apply(lambda row: sum(i > 400 for i in literal_eval(row['200_before'])), axis = 1)
+vacant_crime_df['nonviolent_200_after'] = vacant_crime_df.apply(lambda row: sum(i > 400 for i in literal_eval(row['200_after'])), axis = 1) 
 
 print("STEP 2c: Count crimes 500 meter radius")
-vacant_crime_df['violent_500_before'] = greened_with_crime_df.apply(lambda row: sum(i <= 400 for i in literal_eval(row['500_before'])), axis = 1)
-vacant_crime_df['violent_500_after'] = greened_with_crime_df.apply(lambda row: sum(i <= 400 for i in literal_eval(row['500_after'])), axis = 1)
-vacant_crime_df['nonviolent_500_before'] = greened_with_crime_df.apply(lambda row: sum(i > 400 for i in literal_eval(row['500_before'])), axis = 1)
-vacant_crime_df['nonviolent_500_after'] = greened_with_crime_df.apply(lambda row: sum(i > 400 for i in literal_eval(row['500_after'])), axis = 1) 
+vacant_crime_df['violent_500_before'] = vacant_crime_df.apply(lambda row: sum(i <= 400 for i in literal_eval(row['500_before'])), axis = 1)
+vacant_crime_df['violent_500_after'] = vacant_crime_df.apply(lambda row: sum(i <= 400 for i in literal_eval(row['500_after'])), axis = 1)
+vacant_crime_df['nonviolent_500_before'] = vacant_crime_df.apply(lambda row: sum(i > 400 for i in literal_eval(row['500_before'])), axis = 1)
+vacant_crime_df['nonviolent_500_after'] = vacant_crime_df.apply(lambda row: sum(i > 400 for i in literal_eval(row['500_after'])), axis = 1) 
 
 vacant_crime_df = vacant_crime_df.drop(columns=['100_before', '100_after', '200_before', '200_after', '500_before', '500_after'])
 
 # Drop years 2006 and 2019
-greened_with_crime_df = greened_with_crime_df[(greened_with_crime_df['year'] != 2006) & (greened_with_crime_df['year'] != 2019)]
+vacant_crime_df = vacant_crime_df[(vacant_crime_df['year'] != 2006) & (vacant_crime_df['year'] != 2019)]
 
-greened_with_crime_df.to_csv("data/processed_data/greened_lots_crimes.csv")
+vacant_crime_df.to_csv("data/processed_data/greened_lots_crimes.csv")
